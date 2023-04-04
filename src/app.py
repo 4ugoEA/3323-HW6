e334717df35b51f98e201b5228c05e23e50e3ecd
hugo.e.atayde@gmail.com <hugoe@LAPTOP-FJ9SS67E>
2023-04-04 15:09:17 -0500

    added Student class and File Parser to main app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,67 @@
-import FileParser
-from Student import student
+class student:
+   
+    def __init__(self, id, first_name, last_name, grade, l, f):
+        self._id = id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._grade = grade
+        self._letter = l
+        self.final = f
 
-Rec = FileParser.res
+    def get_id(self):
+        return self._id
+    
+    def set_id(self, id):
+        self._id = id
+    
+    def get_first_name(self):
+        return self._first_name
+    
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+    
+    def get_last_name(self):
+        return self._last_name
+    
+    def set_last_name(self, last_name):
+        self._last_name = last_name
+    
+    def get_grade(self):
+        return self._grade
+    
+    def set_grade(self, grade):
+        self._grade = grade
+    
+    def get_letter(self):
+        return self._letter
+
+    def set_final(self, final):
+        self.final = final
+
+    def get_final(self):
+        return self.final
+
+
+with open("src\input.txt") as f:
+    contents = f.read();
+
+d = "11"
+for line in contents:
+    s =  [d+e for e in contents.split(d) if e]
+
+rest = []
+for sub in s:
+    rest.append(sub.replace("\n", "|"))
+
+rest2 = []
+for sub in rest:
+    rest2.append(sub.replace("||", " "))
+
+res = []
+for sub in rest2:
+    res.append(sub.replace("|", " "))
+
+Rec = res
 n = len(Rec)
 
 astart = 0
