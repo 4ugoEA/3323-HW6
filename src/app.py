60730f23ec30d922954778fafe5411bdc34209b0
hugo.e.atayde@gmail.com <hugoe@LAPTOP-FJ9SS67E>
2023-04-04 11:59:19 -0500

    added some value tests

--- src/app.py
+++ src/app.py
@@ -11,14 +11,19 @@ for x in Rec:
     temp = student(s[0], s[1], s[2], s[3], s[4], 'f')
     Students.append(temp)
 
-print((Students[0]).get_id())
-print((Students[0].get_first_name()))
-print((Students[0].get_last_name()))
-print((Students[0].get_grade()))
-print((Students[0].get_letter()))
+# print((Students[2]).get_id())
+# print((Students[2].get_first_name()))
+# print((Students[2].get_last_name()))
+# print((Students[2].get_grade()))
+# print((Students[2].get_letter()))
+
+gradeSort = sorted(Students, key=lambda x: x.get_grade(), reverse=False)
+
+for x in gradeSort:
+    print(x.get_first_name())
+    print(x.get_grade())
+    print(x.get_final())
     
-
-
 
 #print(Rec[0])
 #print(n)
