55a8204dae49dd1773ba9faee27d4b386e22e45c
hugo.e.atayde@gmail.com <hugoe@LAPTOP-FJ9SS67E>
2023-04-04 10:37:58 -0500

    added working text file parser

--- src/FileParser.py
+++ src/FileParser.py
@@ -1,4 +1,15 @@
 with open("src\input.txt") as f:
     contents = f.read();
 
-print(contents.split("11"))
+d = "11"
+for line in contents:
+    s =  [d+e for e in contents.split(d) if e]
+
+res = []
+for sub in s:
+    res.append(sub.replace("\n", " "))
+
+#print(contents.split("11"))
+
+print(res)
+print(len(res))
