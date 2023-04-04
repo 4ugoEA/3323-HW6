c58460205a9e849584064848e992d707d6573a95
hugo.e.atayde@gmail.com <hugoe@LAPTOP-FJ9SS67E>
2023-04-04 11:22:31 -0500

    adjusted some function names

--- src/Student.py
+++ src/Student.py
@@ -1,25 +1,40 @@
 class student:
-    id = 0
-    first = ""
-    last = ""
-    grade = 0
-    state = "N"
+   
+    def __init__(self, id, first_name, last_name, grade, l, f):
+        self._id = id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._grade = grade
+        self._letter = l
+        self.final = f
 
-    def setId(int):
-        id = int
-
-    def setFirst(str):
-        first = str
-
-    def setLast(str):
-        last = str
-
-    def setGrade(int):
-        grade = int
-
-    def setState(char):
-        state = char
-
+    def get_id(self):
+        return self._id
+    
+    def set_id(self, id):
+        self._id = id
+    
+    def get_first_name(self):
+        return self._first_name
+    
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+    
+    def get_last_name(self):
+        return self._last_name
+    
+    def set_last_name(self, last_name):
+        self._last_name = last_name
+    
+    def get_grade(self):
+        return self._grade
+    
+    def set_grade(self, grade):
+        self._grade = grade
+    
+    def get_letter(self):
+        return self._letter
+        
 
 
 
